6ae21e9aa265db3110fd76b662e2172b6d0c61bb
apple <apple@apples-MacBook-Pro.local>
2020-04-01 16:24:49 +0500

    ledger creation

--- common/models/person.js
+++ common/models/person.js
@@ -26,21 +26,41 @@ module.exports = function(Person) {
                 if (!loc.endTime) {
                     error.push('Missing attribute endTime at index ' + i);
                 }
-                loc['syncToLedger'] = false;
+                loc['syncToLedger'] = true;
                 location.push(loc);
             });
             if (error.length > 0) {
                 error.join();
                 throw error;
             }
+            //personal note : still have to figure -->loc['syncToLedger'] = true;
+            //creating ledgers
+            console.log('check if works===>', PersonInstance.id);
+            await Promise.all(
+                body.location.map(loc => _createLedger(loc, PersonInstance.id))
+            );
 
             const newPerson = await PersonInstance.updateAttributes({
                 location: location
             });
-            if (newPerson) {}
+
             return 'Location Updated';
         } catch (error) {
             throw new Error(error);
         }
     };
 };
+async function _createLedger(loc, id) {
+    try {
+        return await lbApp.models['Ledger'].create({
+            altitude: loc.altitude,
+            startTime: loc.startTime,
+            endTime: loc.endTime,
+            lat: loc.lat,
+            lng: loc.lng,
+            personId: id
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
