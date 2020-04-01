268b3811b01fb3996b2f1a49d67f07f8c4ac58d5
apple <apple@apples-MacBook-Pro.local>
2020-04-01 12:22:45 +0500

    done with person

--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Person) {
+    Person.prototype.updatePerson = async function(body) {
+        var body;
+        try {
+            var PersonInstance = this;
+            var location = PersonInstance.location;
+            body.location.forEach(loc => {
+                loc['syncToLedger'] = false;
+                location.push(loc);
+            });
+            const newPerson = await PersonInstance.updateAttributes({
+                location: location
+            });
+            return newPerson;
+        } catch (error) {
+            console.log(error);
+        }
 
+        return body;
+    };
 };
