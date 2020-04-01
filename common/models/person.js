fe0aa3069b4cd6f6c4f3b665590aad8140c418be
apple <apple@apples-MacBook-Pro.local>
2020-04-01 13:23:14 +0500

    person's remote method's error handling

--- common/models/person.js
+++ common/models/person.js
@@ -4,20 +4,36 @@ module.exports = function(Person) {
     Person.prototype.updatePerson = async function(body) {
         var body;
         try {
+            var error = [];
             var PersonInstance = this;
             var location = PersonInstance.location;
-            body.location.forEach(loc => {
+            body.location.forEach((loc, i) => {
+                if (!loc.lat) {
+                    error.push('Missing attribute lat at index ' + i);
+                }
+                if (!loc.lng) {
+                    error.push('Missing attribute lng at index ' + i);
+                }
+                if (!loc.startTime) {
+                    error.push('Missing attribute startTime at index ' + i);
+                }
+                if (!loc.endTime) {
+                    error.push('Missing attribute endTime at index ' + i);
+                }
                 loc['syncToLedger'] = false;
                 location.push(loc);
             });
+            if (error.length > 0) {
+                error.join();
+                throw error;
+            }
+
             const newPerson = await PersonInstance.updateAttributes({
                 location: location
             });
             return newPerson;
         } catch (error) {
-            console.log(error);
+            throw new Error(error);
         }
-
-        return body;
     };
 };
