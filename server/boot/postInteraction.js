14d0798664e13aa0a187b836669b8ae806569fdb
apple <apple@apples-MacBook-Pro.local>
2020-04-24 19:37:09 +0500

    rn 3mins beofre

--- server/boot/postInteraction.js
+++ server/boot/postInteraction.js
@@ -49,7 +49,9 @@ async function _postingInteractions(app) {
                         },
                     });
                     let ledgers = await cursor.toArray();
+                    console.log(now, 'right now');
                     console.log(
+                        '3 mins before',
                         moment(now).subtract(3, 'minutes').format('MMM DD h:mm A')
                     );
                     console.log(ledgers.length, 'ledger length');
