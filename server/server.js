930fef7637d7e1e950ed0baf4d7c4fb59ad61f3d
apple <apple@apples-MacBook-Pro.local>
2020-05-14 15:17:00 +0500

    done

--- server/server.js
+++ server/server.js
@@ -7,24 +7,24 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-// const path = require('path');
-// const fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 var app = (module.exports = loopback());
-// const https = require('https');
-// const options = {
-//     key: fs.readFileSync(
-//         path.join(
-//             __dirname,
-//             './ssl/keys/a3032_6e279_2171e6f94fa7a9dce6e6641091812392.key'
-//         )
-//     ),
-//     cert: fs.readFileSync(
-//         path.join(
-//             __dirname,
-//             './ssl/certs/biobiz_devbeans_io_a3032_6e279_1620777599_b92f5e8837ec484361de63dcc0f54e86.crt'
-//         )
-//     ),
-// };
+const https = require('https');
+const options = {
+    key: fs.readFileSync(
+        path.join(
+            __dirname,
+            './ssl/keys/a3032_6e279_2171e6f94fa7a9dce6e6641091812392.key'
+        )
+    ),
+    cert: fs.readFileSync(
+        path.join(
+            __dirname,
+            './ssl/certs/biobiz_devbeans_io_a3032_6e279_1620777599_b92f5e8837ec484361de63dcc0f54e86.crt'
+        )
+    ),
+};
 
 app.start = function() {
     // start the web server
@@ -38,22 +38,22 @@ app.start = function() {
         }
     });
 };
-// app.startHttps = function() {
-//     let Httpsserver = null;
+app.startHttps = function() {
+    let Httpsserver = null;
 
-//     Httpsserver = https.createServer(options, app);
+    Httpsserver = https.createServer(options, app);
 
-//     Httpsserver.listen(app.get('port'), function() {
-//         const baseUrl = 'https://' + app.get('host') + ':' + app.get('port');
-//         app.emit('started', baseUrl);
-//         console.log('LoopBack server listening @ %s%s', baseUrl, '/');
-//         if (app.get('loopback-component-explorer')) {
-//             const explorerPath = app.get('loopback-component-explorer').mountPath;
-//             console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-//         }
-//     });
-//     return Httpsserver;
-// };
+    Httpsserver.listen(app.get('port'), function() {
+        const baseUrl = 'https://' + app.get('host') + ':' + app.get('port');
+        app.emit('started', baseUrl);
+        console.log('LoopBack server listening @ %s%s', baseUrl, '/');
+        if (app.get('loopback-component-explorer')) {
+            const explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+    });
+    return Httpsserver;
+};
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -62,6 +62,6 @@ boot(app, __dirname, function(err) {
 
     // start the server if `$ node server.js`
     if (require.main === module) {
-        app.start();
+        process.env.NODE_ENV === 'production' ? app.startHttps() : app.start();
     }
 });
